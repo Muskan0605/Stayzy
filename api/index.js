009974df7337412f78e4960589a73ec642fae354
Muskan Srivastava <132126694+Muskan0605@users.noreply.github.com>
2025-06-01 21:39:10 +0530

    fix: update CORS origin to production URL

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://stayzy-dk96awpiw-muskan-srivastavas-projects-fc207468.vercel.app/",
   })
 );
 // console.log(process.env.MONGO_URL)
